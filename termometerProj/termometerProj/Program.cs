f4cec059cb410354fa8e27f7bb8a1debc79ad4dd
MyNameJaeff <fredrik.andersson@chasacademy.se>
2024-09-10 14:54:41 +0200

    termometer

---
+++ termometerProj/termometerProj/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace termometerProj
+{
+    internal class Program
+    {
+        static void CheckTemp()
+        {
+            Console.WriteLine("Ange temperaturen i Celsius (exit/x för att avsluta):");
+            string message = Console.ReadLine() ?? "20";
+            if (message == "exit" || message == "x") return;
+
+
+            float temperature = float.Parse(message);
+            //Console.WriteLine((temperature >= 30) ? "Jättevarmt" : (temperature < 20) ? "Hösttemperatur" : "Varmt");
+
+            if (temperature >= 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Jättevarmt");
+            }
+            else if (temperature < 20)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Hösttemperatur");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Varmt");
+            }
+            CheckTemp();
+        }
+        static void Main(string[] args)
+        {
+            CheckTemp();
+        }
+    }
+}
